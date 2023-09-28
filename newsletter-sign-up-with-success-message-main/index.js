cbcf9654133e4a291d230f13cc279d278ae9c576
cc201013 <bedo.faruk13@gmail.com>
2023-09-28 22:17:54 +0200

    first commit

---
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -0,0 +1,42 @@
+/**
+ * Dismiss success dialog and navigate back to the news form
+ */
+function dismissSuccessDialog() {
+  const successDialog = document.getElementById("success-dialog");
+  const newsForm = document.getElementById("newsForm");
+  successDialog.style.display = "none";
+  newsForm.style.display = "inherit";
+}
+
+/**
+ * Subscribe mail and navigate to success dialog
+ */
+function subscribeMail() {
+  const successDialog = document.getElementById("success-dialog");
+  const newsForm = document.getElementById("newsForm");
+  successDialog.style.display = "inherit";
+  newsForm.style.display = "none";
+}
+
+/**
+ * Validate the email input filed and make sure the user only enter valid email
+ */
+function validateEmail() {
+  const emailInput = document.getElementById("email");
+  const emailInputError = document.getElementById("error");
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const isValidEmail = emailPattern.test(emailInput.value);
+
+  if (isValidEmail) {
+    subscribeMail();
+    emailInput.value = "";
+    emailInput.classList.remove("invalid");
+    emailInputError.style.display = "none";
+    emailInput.setAttribute("aria-invalid", "false");
+    emailInput.removeAttribute("aria-describedby");
+  } else {
+    emailInput.classList.add("invalid");
+    emailInput.setAttribute("aria-invalid", "true");
+    emailInputError.style.display = "inherit";
+  }
+}
